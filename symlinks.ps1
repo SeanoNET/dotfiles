660ce10e9d6b8de0028edcbdf25a2b6eabdb7919
Sean O <sean.p.oloughlin@outlook.com>
2025-04-01 12:48:11 +0800

    removed duplicate files

--- symlinks.ps1
+++
@@ -1,3 +0,0 @@
-$documents = [Environment]::GetFolderPath("MyDocuments")
-New-Item -Path $documents\PowerShell\Microsoft.PowerShell_profile.ps1 -ItemType SymbolicLink -Value (Get-Item ".\PowerShell\Microsoft.PowerShell_profile.ps1").FullName
-New-Item -Path $documents\PowerShell\seanonetdev.omp.json -ItemType SymbolicLink -Value (Get-Item ".\PowerShell\seanonetdev.omp.json").FullName
